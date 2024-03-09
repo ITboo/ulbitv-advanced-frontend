b5dc8aec0a198b500effad1bc101ee0f0d284d8f
ITboo <annaacdc@tut.by>
2024-03-09 22:43:11 +0300

    chore: add router config

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>{Object.values(routeConfig).map(({element,path})=>(
+        <Route path={path} element={element} key={path}/>
+      ))}</Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
